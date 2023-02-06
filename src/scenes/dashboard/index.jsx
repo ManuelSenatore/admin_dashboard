1c17f81b19ed91c5b3ff38a5c7da8de2dc1fc6c0
ManuelSenatore <106093444+ManuelSenatore@users.noreply.github.com>
2023-02-06 13:04:29 +0100

    sidebar

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,10 +1,14 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Header from '../../components/Header'
 
 const Dashboard = () => {
   return (
-    <div>
-      
-    </div>
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Header title="DASHBOARD" subtitle="Welcome to your Dashboard"/>
+      </Box>
+    </Box>
   )
 }
 
